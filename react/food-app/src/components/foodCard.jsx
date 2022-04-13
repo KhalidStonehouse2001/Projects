b7f67bc61e12ae08c0f57b91ce419c169cb164fe
KStonehouse2001 <khalidstonehouse12@outlook.com>
2022-04-13 17:17:55 +0100

    removed commented out test from foodCard.test

--- react/food-app/src/components/foodCard.jsx
+++ react/food-app/src/components/foodCard.jsx
@@ -2,17 +2,14 @@ import React, { useEffect, useState } from "react";
 
 const Foodcard = ({ food }) => {
   return (
-      <div className="App">
-        <li data-testid="food-card-item" key={food.id} className="box">
-          <p>Food Name: {food.name}</p>
-          <p>Place Of Origin: {food.origin}</p>
-          <p>Star Rating: {food.starRating} ⭐️</p>
-          <img
-            className="image"
-            src={food.image}
-          />
-        </li>
-      </div>
+    <div className="App">
+      <li data-testid="food-card-item" key={food.id} className="box">
+        <p>Food Name: {food.name}</p>
+        <p>Place Of Origin: {food.origin}</p>
+        <p>Star Rating: {food.starRating} ⭐️</p>
+        <img className="image" src={food.image} />
+      </li>
+    </div>
   );
 };
 
