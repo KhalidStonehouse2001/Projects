d113b63206c5b4a132a90643db07a0b54b3929e3
KStonehouse2001 <khalidstonehouse12@outlook.com>
2022-04-13 17:06:07 +0100

    formatted code with prettier

--- react/food-app/src/App.js
+++ react/food-app/src/App.js
@@ -1,48 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import Foodcard from './components/foodCard'
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Foodcard from "./components/foodCard";
+import "./App.css";
 
 function App() {
-  const [food, setFood] = useState([])
-  const [searchTerm, setSearchTerm] = useState('')
+  const [food, setFood] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  
-  const  onChange = (e) => {
-    setSearchTerm(e.target.value)
-}
-  const  onSubmit = (e) => {
-   e.preventDefault()
-
-}
+  const onChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
   useEffect(() => {
-    fetch('https://pair-programming-test.s3.eu-west-2.amazonaws.com/cuisine.json').then((res) => res.json()).then((data) => setFood(data))
-  }, [])
+    fetch(
+      "https://pair-programming-test.s3.eu-west-2.amazonaws.com/cuisine.json"
+    )
+      .then((res) => res.json())
+      .then((data) => setFood(data));
+  }, []);
 
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="form">
+        <label>Search For Your Favourite Food</label>
+        <input
+          className="input-search"
+          onChange={onChange}
+          type="text"
+          placeholder="UK, Fish, Pizza ..."
+        />
+      </form>
 
-return <div>
-  <form onSubmit={onSubmit} className='form'>
-          <label >Search For Your Favourite Food</label>
-          <input className='input-search' onChange={onChange} type='text' placeholder='UK, Fish, Pizza ...'/>
-        </form>
-
-  <ul className='food-ul'>
-    {food.map((item) => {
-      if(searchTerm){
-        if(item.name.includes(searchTerm) || item.origin.includes(searchTerm)){
-          return <Foodcard food={item} key={item.id}/>
-        }
-      } else if(!searchTerm){
-        return <Foodcard food={item} key={item.id}/>
-
-      }
-    })}
-  </ul>
-
-</div> 
+      <ul className="food-ul">
+        {food.map((item) => {
+          if (searchTerm) {
+            if (
+              item.name.includes(searchTerm) ||
+              item.origin.includes(searchTerm)
+            ) {
+              return <Foodcard food={item} key={item.id} />;
+            }
+          } else if (!searchTerm) {
+            return <Foodcard food={item} key={item.id} />;
+          }
+        })}
+      </ul>
+    </div>
+  );
 }
-
-
-
-
 
 export default App;
