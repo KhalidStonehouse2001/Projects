a360e401ffe11e8811e54e3a9765a242da6b5dc1
KStonehouse2001 <khalidstonehouse12@outlook.com>
2022-04-13 12:41:40 +0100

    food react app working

---
+++ react/food-app/src/App.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+import Foodcard from './components/foodCard'
+import './App.css';
+
+function App() {
+return <div>
+  <Foodcard/>
+</div> 
+}
+
+export default App;
---
+++ react/food-app/src/components/foodCard.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+
+const Foodcard = () => {
+  const [food, setFood] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+    useEffect(() => {
+      fetch('https://pair-programming-test.s3.eu-west-2.amazonaws.com/cuisine.json').then((res) => res.json()).then((data) => setFood(data))
+    }, [])
+  
+    const  onChange = (e) => {
+      setSearchTerm(e.target.value)
+  }
+    const  onSubmit = (e) => {
+     e.preventDefault()
+  
+  }
+  
+    return (
+      <div>
+  
+        <form onSubmit={onSubmit} className='form'>
+          <label >Search </label>
+          <input onChange={onChange} type='text' placeholder='Burger, Pizza ...'/>
+        </form>
+      <div className="App">
+          {food.map((item) => {
+            if(searchTerm){
+             if(item.name === searchTerm || item.origin === searchTerm) {
+              return (
+                <li key={item.id} className='box'>
+                <p>Food Name: {item.name}</p>
+               <p>Place Of Origin: {item.origin}</p>
+               <p>Star Rating: {item.starRating} ⭐️</p>
+               <img className='image' src={item.image}/>
+            </li> )
+             }  
+               
+            }  else if(!searchTerm) {
+            return <li key={item.id} className='box'>
+                <p>Food Name: {item.name}</p>
+               <p>Place Of Origin: {item.origin}</p>
+               <p>Star Rating: {item.starRating} ⭐️</p>
+               <img className='image' src={item.image}/>
+            </li>
+  
+            }
+              
+          })}
+      </div>
+  
+      </div>
+    );
+}
+
+export default Foodcard;
+
+
+
