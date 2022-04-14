5fa0bc36f102c2fd36b2215868ff2e87d00fa7a4
KStonehouse2001 <khalidstonehouse12@outlook.com>
2022-04-14 12:36:00 +0100

    First test added and passing - Food name

--- react/food-app/src/components/foodCard.test.jsx
+++ react/food-app/src/components/foodCard.test.jsx
@@ -1,1 +1,18 @@
-
+import {render, screen } from '@testing-library/react'
+import Foodcard from './foodCard';
+describe('Testing Food Card Component', () => {
+    describe('Food Name ', () => {
+        it('renders Food Name', () => {
+            const foodMockData = {
+                name: 'Mock Food',
+                image: 'url',
+                origin: 'Manchester',
+                starRating: 5
+            }
+            render(<Foodcard food={foodMockData}/>)
+            const foodNameElement = screen.getByText('Food Name: Mock Food')
+        });
+        
+    });
+    
+});
