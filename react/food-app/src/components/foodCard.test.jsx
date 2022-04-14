33bde268ebe7b1b71a2c7a720169fa31d27f75dc
KStonehouse2001 <khalidstonehouse12@outlook.com>
2022-04-14 13:57:47 +0100

    All tests added for food card and passing

--- react/food-app/src/components/foodCard.test.jsx
+++ react/food-app/src/components/foodCard.test.jsx
@@ -1,35 +1,56 @@
-import {render, screen } from '@testing-library/react'
-import Foodcard from './foodCard';
-describe('Testing Food Card Component', () => {
-    describe('Food Name ', () => {
-        it('Renders Food Name', () => {
-            const foodMockData = {
-                name: 'Mock Food',
-                image: 'url',
-                origin: 'Manchester',
-                starRating: 5
-            }
-            render(<Foodcard food={foodMockData}/>)
-            const foodNameElement = screen.getByText('Food Name: Mock Food')
-            expect(foodNameElement).toBeInTheDocument();
-        });
-        
+import { render, screen } from "@testing-library/react";
+import Foodcard from "./foodCard";
+describe("Testing Food Card Component", () => {
+  describe("Food Name ", () => {
+    it("Renders Food Name", () => {
+      const foodMockData = {
+        name: "Mock Food",
+        image: "url",
+        origin: "Manchester",
+        starRating: 5,
+      };
+      render(<Foodcard food={foodMockData} />);
+      const foodNameElement = screen.getByText("Food Name: Mock Food");
+      expect(foodNameElement).toBeInTheDocument();
     });
-    describe('Food Image', () => {
-        it('Renders Food Image', () => {
-            const foodMockData = {
-                name: 'Mock Food',
-                image: 'url',
-                origin: 'Manchester',
-                starRating: 5,
-            }
-            render(<Foodcard food={foodMockData}/>)
-            const foodImageElement = screen.getByTestId('food-card-item')
-            expect(foodImageElement).toBeInTheDocument();
-
-        });
-        
+  });
+  describe("Food Image", () => {
+    it("Renders Food Image", () => {
+      const foodMockData = {
+        name: "Mock Food",
+        image: "url",
+        origin: "Manchester",
+        starRating: 5,
+      };
+      render(<Foodcard food={foodMockData} />);
+      const foodImageElement = screen.getByTestId("food-card-item");
+      expect(foodImageElement).toBeInTheDocument();
     });
-    
-    
+  });
+  describe("Food Rating", () => {
+    it("Renders Food Rating", () => {
+      const foodMockData = {
+        name: "Mock Food",
+        image: "url",
+        origin: "Manchester",
+        starRating: 4,
+      };
+      render(<Foodcard food={foodMockData} />);
+      const foodRatingElement = screen.getByText("Star Rating: 4 ⭐️");
+      expect(foodRatingElement).toBeInTheDocument();
+    });
+  });
+  describe("Food Origin", () => {
+    it("Renders Food Origin", () => {
+      const foodMockData = {
+        name: "Mock Food",
+        image: "url",
+        origin: "Manchester",
+        starRating: 4,
+      };
+      render(<Foodcard food={foodMockData} />);
+      const foodOriginElement = screen.getByText("Place Of Origin: Manchester");
+      expect(foodOriginElement).toBeInTheDocument();
+    });
+  });
 });
