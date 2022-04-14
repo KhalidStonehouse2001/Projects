a449e536f689d78f69af05c5483dbf941598d50f
KStonehouse2001 <khalidstonehouse12@outlook.com>
2022-04-14 12:41:36 +0100

    Second Test added & Expect statement - Food Image

--- react/food-app/src/components/foodCard.test.jsx
+++ react/food-app/src/components/foodCard.test.jsx
@@ -2,7 +2,7 @@ import {render, screen } from '@testing-library/react'
 import Foodcard from './foodCard';
 describe('Testing Food Card Component', () => {
     describe('Food Name ', () => {
-        it('renders Food Name', () => {
+        it('Renders Food Name', () => {
             const foodMockData = {
                 name: 'Mock Food',
                 image: 'url',
@@ -11,8 +11,25 @@ describe('Testing Food Card Component', () => {
             }
             render(<Foodcard food={foodMockData}/>)
             const foodNameElement = screen.getByText('Food Name: Mock Food')
+            expect(foodNameElement).toBeInTheDocument();
         });
         
     });
+    describe('Food Image', () => {
+        it('Renders Food Image', () => {
+            const foodMockData = {
+                name: 'Mock Food',
+                image: 'url',
+                origin: 'Manchester',
+                starRating: 5,
+            }
+            render(<Foodcard food={foodMockData}/>)
+            const foodImageElement = screen.getByTestId('food-card-item')
+            expect(foodImageElement).toBeInTheDocument();
+
+        });
+        
+    });
+    
     
 });
